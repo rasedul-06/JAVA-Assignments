d117aaf983be01cc379f6cbf706d5f49a193f998
Rasedul Islam <128040157+rasedul-06@users.noreply.github.com>
2025-05-18 22:36:33 +0600

    Add files via upload

---
+++ Assignments/Assignment 2/Product.java
@@ -0,0 +1,26 @@
+package Assignment2;
+
+public class Product {
+    public static void main(String[] args){
+        int id;
+        String title;
+        String price;
+        String description;
+        String category;
+
+        id=101;
+        System.out.println("Id is: "+id);
+
+        title="Iphone15.";
+        System.out.println("Title is: "+title);
+
+        price="1895 Euros.";
+        System.out.println("Price is: "+price);
+
+        description="Perfect product with best image quality.";
+        System.out.println("Description is: "+description);
+
+        category="Phone.";
+        System.out.println("Category is: "+category);
+    }
+}
