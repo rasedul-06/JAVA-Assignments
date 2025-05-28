60e14f6df0a48d6b5f2e9566f27c93500602dc1f
Rasedul Islam <128040157+rasedul-06@users.noreply.github.com>
2025-05-28 13:10:12 +0600

    Create class_main_method.java

---
+++ Assignments/Assignment 1/class_main_method.java
@@ -0,0 +1,11 @@
+package Assignment1;
+// create class and main method
+public class class_main_method {
+    public static void main(String[] args) {
+        System.out.println("Id: 101");
+        System.out.println("Tittle: Iphone 15.");
+        System.out.println("Price: 1,05,000");
+        System.out.println("Description: Prefer product for best image quality.");
+        System.out.print("Category: Phone.");
+    }
+}
